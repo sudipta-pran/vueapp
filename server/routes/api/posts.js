c3a54c76b2084c91c209625e6916dd7f892cee18
Sudipta Bhattacharyya <jinturocks@gmail.com>
2018-10-09 17:26:28 +0530

    initial commit

---
+++ server/routes/api/posts.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mongodb = require('mongodb')
+
+const router = express.Router()
+
+//Get Posts
+router.get('/', async (req,res) => {
+    const posts = await loadPostsCollection()
+    res.send(await posts.find({}).toArray())
+})
+
+//Add Posts
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection()
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    })
+    res.status(201).send()
+})
+
+//Delete Posts
+router.delete('/:id', async (req,res) => {
+    const posts = await loadPostsCollection()
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)})
+    res.status(200).send()
+})
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient
+        .connect('mongodb://abc123:abc123@ds125713.mlab.com:25713/vue_express', {
+            useNewUrlParser: true
+        })
+
+    return client.db('vue_express').collection('posts')    
+}
+
+module.exports = router
